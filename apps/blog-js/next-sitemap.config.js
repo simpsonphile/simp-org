ee0be822a4fdbfb4ee1d164873022ba72b72186f
simpsonphile <simpsonphile@gmail.com>
2023-02-14 19:09:09 +0100

    changes

--- apps/blog-js/next-sitemap.config.js
+++ apps/blog-js/next-sitemap.config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_BASE_PATH,
   generateRobotsTxt: true,
   sourceDir: 'dist/apps/blog-js/.next',
-  outDir: 'dist/apps/blog-js/public',
+  outDir: 'dist/apps/blog-js/exported/',
 };
