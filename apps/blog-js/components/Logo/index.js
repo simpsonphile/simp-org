80436eade66418afc9859afec37aa0459d8cb34b
simpsonphile <simpsonphile@gmail.com>
2023-02-17 22:32:30 +0100

    changes

--- apps/blog-js/components/Logo/index.js
+++ apps/blog-js/components/Logo/index.js
@@ -1,4 +1,9 @@
+import Link from 'next/link';
 import styles from './index.module.scss';
 
-const Logo = () => (<p className={styles.Logo}>noidea</p>)
+const Logo = () => (
+  <p className={styles.Logo}>
+    <Link href="/">noidea</Link>
+  </p>
+);
 export default Logo;
