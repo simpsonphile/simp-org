58837476b5acdd0d8c3dba59ea0bc6d462416d10
simpsonphile <simpsonphile@gmail.com>
2023-02-20 18:57:28 +0100

    Add copy functionality to shortened addresses

--- apps/gamble-eth/src/app/components/ShortAddress/index.js
+++ apps/gamble-eth/src/app/components/ShortAddress/index.js
@@ -1,8 +1,38 @@
 import { shortenAddress } from '@usedapp/core';
-import { Tooltip } from '@chakra-ui/react';
-
+import { HStack, Tooltip } from '@chakra-ui/react';
+import { CopyIcon } from '@chakra-ui/icons';
+import { Button } from '@chakra-ui/react';
+import { useCopyToClipboard } from 'react-use';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+} from '@chakra-ui/react';
 const ShortAddress = ({ children }) => {
-  return <Tooltip label={children}>{shortenAddress(children)}</Tooltip>;
+  const [, copy] = useCopyToClipboard();
+
+  const onClick = () => {
+    copy(children);
+  };
+
+  return (
+    <Tooltip label={children}>
+      <HStack>
+        <span>{shortenAddress(children)}</span>
+        <Popover size="sm">
+          <PopoverTrigger>
+            <Button size="xs" onClick={onClick}>
+              <CopyIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverBody as="">copied</PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </HStack>
+    </Tooltip>
+  );
 };
 
 export default ShortAddress;
